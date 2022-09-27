e9eff5d503e4b1e4acdf6533c004cdee0d63bfa8
UgohDiegoKC <diegokingsley14@gmail.com>
2022-09-27 11:35:58 +0100

    Configure remote state

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "s3" {
+    bucket = "terraform-remote-state-dieg007"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-remote-state"
+  }
 }
 
 provider "aws" {
