c0d4900947dbc0a550e10bb2563ccf344a0c9f4b
UgohDiegoKC <diegokingsley14@gmail.com>
2022-12-08 05:54:37 +0100

    Created secret

--- level2/secret.tf
+++ level2/secret.tf
@@ -1,12 +1,12 @@
-data "aws_secretmanager_secret" "rds_password" {
+data "aws_secretsmanager_secret" "rds_password" {
     name = "main/rds/password"
 }
 
-data "aws_secretmanager_secret_version" "rds_password" {
-    secret_id = data.aws_secretmanager_secret.rds_password.id
+data "aws_secretsmanager_secret_version" "rds_password" {
+    secret_id = data.aws_secretsmanager_secret.rds_password.id
 }
 
 locals {
-    rds_password = jsondecode(data.aws_secretmanager_secret_version.rds_password.secret_string) ["password"]
+    rds_password = jsondecode(data.aws_secretsmanager_secret_version.rds_password.secret_string) ["password"]
 }
 
